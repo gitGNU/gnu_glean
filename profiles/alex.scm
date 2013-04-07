95999e28a72474860c5ddcf951f4614194f0f111
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2013-04-08 01:02:59 +0200

    Implemented use of new gmodule-manager. - removed use-module calls to individual gmodules that are active in this profile (no longer necessary because profile does not contain full gmodule-objects, but keyword gmodule IDs, which are expanded into gmodule objects using gmodule-manager). - Simplified active-modules element of profile: now a simple list of gmodule-IDs, rather than a quasiquoted list of gmodule-objects.

--- profiles/alex.scm
+++ profiles/alex.scm
@@ -3,20 +3,18 @@
 (define-module (profiles alex)
   #:use-module (guilecraft gprofiles)
   #:use-module (guilecraft scorecards)
-  #:use-module (modules git)
-  #:use-module (modules obnam)
   #:export (alex-profile))
 
 (define alex-profile
   (gprof_make-profile "130215alex"
 		      "alex"
-		      `(,git-gmodule ,obnam-gmodule)
-		      `(,(scc_make-scorecard-datum git-gmodule
+		      '(git)
+		      `(,(scc_make-scorecard-datum 'git
 						   'git-branch
 						   0)
-			,(scc_make-scorecard-datum git-gmodule
+			,(scc_make-scorecard-datum 'git
 						   'git-status
 						   2)
-			,(scc_make-scorecard-datum git-gmodule
+			,(scc_make-scorecard-datum 'git
 						   'git-add
 						   5))))
