0f25e373bb362472bef769a2f7b3376047ea9ca3
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2013-04-21 01:59:07 +0200

    Add multi-choice problem-type and upgrade open-problems
    - multi-choice-problems.scm: new problem-type definition and provider.
    - open-problems.scm: change structure to definition/provider model and add call to add to problem-type-manager.

---
+++ guilecraft/types/multi-choice-problems.scm
@@ -0,0 +1,92 @@
+;;; guilecraft --- .         -*- coding: utf-8 -*-
+
+(define-module (guilecraft types multi-choice-problems)
+  #:use-module (guilecraft data-manager)
+  #:use-module (guilecraft problem-type-manager)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-9)
+  #:export (multi-choice-problem-provider
+	    make-multi-choice-problem))
+
+(define-record-type <multi-choice-problem>
+  (make-problem question solution options)
+  multi-choice-problem?
+  (question get-question)
+  (solution get-solution)
+  (options get-options))
+
+(define get-challenge
+  (lambda (multi-choice-problem)
+    (string-join (list (get-question multi-choice-problem)
+		       (turn-options (get-options multi-choice-problem))))))
+
+(define turn-options
+  (lambda (options)
+    "options is a list containing all answer options. Turn options
+walks through that list and creates a string formatted for output."
+    (define turn-option
+      (lambda (option)
+	(string-append (get-option-symbol option) ") "
+		       (get-option-description option) "\n")))
+    (string-join (map turn-option options))))
+
+;; Helper Functions:
+(define assess
+  (lambda (player-answer multi-choice-problem-solution)
+    "Evaluate @var{player-answer} against @var{multi-choice-problem-solution}
+and return #t if correct or #f if incorrect."
+    (mcp_equal? player-answer multi-choice-problem-solution)))
+(define make-option
+  (lambda (symbol description)
+    "Create an option used to populate the option fields in <multi-choice-problem>"
+    (cons symbol description)))
+(define get-option-symbol
+  (lambda (option)
+    "Return the symbol associated with a multi-choice-option."
+    (car option)))
+(define get-option-description
+  (lambda (option)
+    (cdr option)))
+
+(define mcp_equal?
+  (lambda (player-answer multi-choice-problem-solution)
+    "Predicate equivalence for @var{player-answer} and
+@var{multi-choice-problem-solution}. This is separated into a separate module
+to maximise the resilience of the superstructure against low-level changes."
+    (if (or (equal? player-answer (get-option-symbol multi-choice-problem-solution))
+	    (equal? player-answer (get-option-description multi-choice-problem-solution)))
+	#t
+	#f)))
+
+;; this will be the central interface providing an id method, asses
+;; and get-problem interfaces, as well as possibly a print method
+(define multi-choice-problem-provider
+  (lambda (message . args)
+    "Provide an open problem type interface.  Inserted into
+problem-type-manager, this procedure provides all actions that need to
+be provided by a problem type.
+
+The car of args should always be the problem."
+    (cond ((eq? message 'get-challenge)
+	   (get-challenge (car args)))
+	  ((eq? message 'assess-answer)
+	   (assess (cadr args) (get-solution (car args))))
+	  ((eq? message 'make)
+	   (make-problem (car args)  ; question
+			 (cadr args) ; solution
+			 (car (cddr args)) ; options
+			 ))
+	  ((eq? message 'print-problem)
+	   (format #t "print not yet, using display:\n ~s" (car args))))))
+
+(define make-multi-choice-problem
+  (lambda (question solution . options)
+    "Convenience procedure for creating multi-choice-problems when defining modules."
+    (multi-choice-problem-provider 'make question solution options)))
+
+(ptm_add-problem-type multi-choice-problem-provider (make-problem
+						     "question"
+						     "solution"
+						     '("option1"
+						       "option2"
+						       "option3")))
--- guilecraft/types/open-problems.scm
+++ guilecraft/types/open-problems.scm
@@ -1,57 +1,58 @@
-;;; guilecraft --- .         -*- coding: utf-8 -*-
+;;; guilecraft --- fast learning tool.         -*- coding: utf-8 -*-
 
 (define-module (guilecraft types open-problems)
+  #:use-module (guilecraft problem-type-manager)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-9)
-  #:export (;; open-problem record functions
-	    op_assess
-	    op_open-problem?
-	    op_make-open-problem
-	    op_get-challenge
-	    op_get-solution))
+  #:export (open-problem-provider
+	    make-open-problem))
 
-;;;
-;; Define Problems
-;;;
-
-;;;
-;; Problems are the lowest level data-type used in gmodule definition.
-;; They provide a challenge and some form of correct solution.
-;; We aim to provide a great variety of problem-types such as:
-;; - open-problems: challenges expecting a freely typed answer that 
-;; matches the provided solution
-;; - multiple-choice: challenges that offer a number of solutions from
-;; which the user chooses.
-;; - dynamic: challenges are generated dynamically according to rules,
-;; the user must type an answer that matches the algorithmically
-;; generated solution.
-;;
-;; Problems should also be able to provide their challenges using
-;; different media types: text, audio, video.
-
-;; Open-problem: an individual problem record of the open problem
-;; variety.
 (define-record-type <open-problem>
-  (op_make-open-problem challenge solution)
-  op_open-problem?
+  (make-problem challenge solution)
+  open-problem?
   ; Challenges are posed to players to provide a solution to.
-  (challenge op_get-challenge)      ; String
+  (challenge get-challenge)      ; String
   ; Solutions are what the player's response is assessed against.
-  (solution op_get-solution)        ; String 
+  (solution get-solution)        ; String 
   ) 
 
 ;; Helper Functions:
-(define op_assess
-  (lambda (player-answer open-problem-solution)
+(define assess
+  (lambda (open-problem-solution player-answer)
     "Evaluate @var{player-answer} against @var{open-problem-solution}
 and return #t if correct or #f if incorrect."
-    (op_eq? player-answer open-problem-solution)))
+    (eq? open-problem-solution player-answer)))
 
-(define op_eq?
-  (lambda (player-answer open-problem-solution)
+(define eq?
+  (lambda (open-problem-solution player-answer)
     "Predicate equivalence for @var{player-answer} and
 @var{open-problem-solution}. This is separated into a separate module
 to maximise the resilience of the superstructure against low-level changes."
-    (if (equal? player-answer open-problem-solution)
+    (if (equal? open-problem-solution player-answer)
 	#t
 	#f)))
+
+;; this will be the central interface providing an id method, asses
+;; and get-problem interfaces, as well as possibly a print method
+(define open-problem-provider
+  (lambda (message . args)
+    "Provide an open problem type interface.  Inserted into
+problem-type-manager, this procedure provides all actions that need to
+be provided by a problem type.
+
+The car of args should always be the problem."
+    (cond ((eq? message 'get-challenge)
+	   (get-challenge (car args)))
+	  ((eq? message 'assess-answer)
+	   (assess (cadr args) (get-solution (car args))))
+	  ((eq? message 'make)
+	   (make-problem (car args) (cadr args)))
+	  ((eq? message 'print-problem)
+	   (format #t "print not yet, using display:\n ~s" (car args))))))
+
+(define make-open-problem
+  (lambda (question solution)
+    "Convenience procedure for creating open-problems when defining modules."
+    (open-problem-provider 'make question solution)))
+
+(ptm_add-problem-type open-problem-provider (make-problem "problem" "solution"))
