9d5bf2df370008711cb4c34bd49fb0da3a374fab
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2013-04-21 01:46:53 +0200

    Add new problem-type managing table (using data-manager).
    - data-manager.scm: Make data-manager more generic so we can use it to provide a dynamic registry table for problem-types used by gmodules loaded into guilecraft at runtime.
    - problem-type-manager.scm: New file providing the problem-type-manager (instance of data-manager). It also provides:
      - assess-answer: a generic procedure using the new problem-type-manager to, given any challenge and a player answer, evaluate the player answer according to rules specified by the challenge's type.
      - get-challenge: a generic procedure using the new problem-type-manager to, given any problem-type, retrieve the challenge associated with it.

--- guilecraft/data-manager.scm
+++ guilecraft/data-manager.scm
@@ -2,40 +2,52 @@
 
 ;;; Commentary:
 ;;
-;; Gmodule-Manager defines an interface to centrally store all known
-;; gmodules. The table is indexed on gmodule-id and it returns a
-;; gmodule object.
-;; Gmodule-Manager also defines an interface 
+;; data-manager provides the dman_make-data-manager procedure, which,
+;; much like make-whirligig, supplies a procedure with local state to
+;; track dynamic server side content.
+;; The procedure returned by make-data-manager maintains a table,
+;; indexed by an arbitrary identifier, retrieved through the procedure
+;; @var{identifier-getter}, passed to make-data-manager when called.
+;; Once created, the new data-manager accepts 'put, get and 'list as
+;; messages. 'put expects a data-type provider and a data-type
+;; prototype. An identifier is extracted from data-type prototype
+;; using identifier-getter, after which, each time the data-manager
+;; procedure is called with 'get data-type object, the data-type
+;; provider is used to manipulate it.
+;;
+;; Currently data-manager procedures are used by gmodule-manager, to
+;; keep track of, and make use of the gmodules known to guilecraft;
+;; and by problem-type-manager, to keep track of, and make use of the
+;; problem-types (the templates for problems used in gmodules) known
+;; to guilecraft.
+;;
+;; Code:
 
 (define-module (guilecraft data-manager)
   #:use-module (guilecraft gmodules)
+
   #:export (dman_add-gmodule
 	    dman_get-gmodule
-	    dman_gmodule-manager))
-
-(define first-arg
-  (lambda (args)
-    (car args)))
-(define first-tag
-  (lambda (data-type-table)
-    (car (car data-type-table))))
-(define first-data-type-object
-  (lambda (data-type-table)
-    (cdr (car data-type-table))))
-(define rest-of-table
-  (lambda (data-type-table)
-    (cdr data-type-table)))
+	    dman_data-manager))
 
 (define dman_data-manager
-  (lambda ()
+  (lambda (identifier-getter)
     "Generic data-managing table interface. Intended as a factory to
 spawn tables to handle specific data-types (e.g. gmodules or
-problem-types)."
+problem-types).
+
+identifier-getter must be a procedure that, when applied to a
+data-type object managed by the data-manager instance, returns the
+object's identifier (e.g. its type for problem-types or ID tag for
+gmodules)."
+
     (let ([data-type-table '()])
       (lambda (message . args)
-	"Takes 'put or 'get as message, which either installs a
-data-type (e.g. gmodule, problem-type) into the table or returns a
-data-type from the table: 
+	"Takes 'put, 'get or 'list as message. Either (with 'put
+data-object-prototype) installs a data-type (e.g. gmodule,
+problem-type) into the table, or, when called with 'get and an indexed
+data-type identifier returns content or a content provider for that
+data-type:
 dman_data-manager 'put data-type -> appends data-type object,
 indexed by its data-type-id to the data-type-table and returns 'done or
 #f. 
@@ -45,42 +57,66 @@ associated with data-type-id or #f if no data-type object can be found.
 data-type-table is the central, canonical repository of known data-types. All
 data-types should implement a call to their respective data-manager's
 'put function, so that they can be added to the that manager on load."
+	(define first-id
+	  (lambda (data-type-table)
+	    (car (car data-type-table))))
+	(define first-data-type-object
+	  (lambda (data-type-table)
+	    (cdr (car data-type-table))))
+	(define rest-of-table
+	  (lambda (data-type-table)
+	    (cdr data-type-table)))
+
 	(define data-type-getter
-	  (lambda (arg tmp-data-type-table)
-	    "Return data-type-object if arg is found in tmp-data-type-table and #f
-if not. Recurses through tmp-data-type-table."
+	  (lambda (id tmp-data-type-table)
+	    "Return content associated with id from the table, if id
+is an index in the table. Return #f otherwise."
 	    (cond ((null? tmp-data-type-table)
 		   #f)
-		  ((eq? arg (first-tag tmp-data-type-table))
+		  ((eq? id (first-id tmp-data-type-table))
 		   (first-data-type-object tmp-data-type-table))
-		  (else (data-type-getter arg (rest-of-table
+		  (else (data-type-getter id (rest-of-table
 					       tmp-data-type-table))))))
 
 	(define data-type-putter
-	  (lambda (data-type-object data-type-table)
-	    "Returns a new data-type-table with the new data-type-object added."
-	    (cons (cons (gmod_get-id data-type-object) data-type-object) data-type-table)))
+	  (lambda (args identifier-getter old-data-type-table)
+	    "Returns a new table with the new content
+(data-type-interface) added by index id, retrieved through applying
+identifier-getter to the data-type-prototype."
+	    (let ([data-interface (car args)] [data-prototype (cadr args)])
+	      (cons (cons (identifier-getter data-prototype) data-interface) old-data-type-table))))
+
 
 	(cond ((eq? message 'get)
-	       (data-type-getter (first-arg args) data-type-table))
+	       (let ([result (data-type-getter (car args)
+					       data-type-table)])
+		 (if result
+		     result
+		     (error "data-manager 'get: index not found: "
+			    (car args)))))
 	      ((eq? message 'put)
 	       (begin
-		 (set! data-type-table (data-type-putter (first-arg args) data-type-table))
+		 (set! data-type-table (data-type-putter args identifier-getter data-type-table))
 		 #t))
 	      ((eq? message 'list)
 	       data-type-table)
-	      (else (error "data-type-manager: unknown message: " message)))))))
+
+	      (else (error "data-manager: unknown message: " message)))))))
 
 (define dman_add-gmodule
   (lambda (gmodule-object)
     "Convenience procedure to add a given gmodule to the
-gmodule-manager."
-    (dman_gmodule-manager 'put gmodule-object)))
+gmodule-manager.
+
+gmodule-manager is an instance of data-manager. It stores the
+gmodule-object indexed by its gmodule-id, derived using gmod_get-id."
+    (gmodule-manager 'put gmodule-object gmodule-object)))
 
 (define dman_get-gmodule
-  (lambda (gmodule-tag)
+  (lambda (gmodule-id)
     "Convenience procedure to retrieve a given gmodule from the active
-gmodule-table."
-    (dman_gmodule-manager 'get gmodule-tag)))
+gmodule-table stored in gmodule-manager."
+    (gmodule-manager 'get gmodule-id)))
 
-(define dman_gmodule-manager (dman_data-manager))
+; define a data-manager instance using the result of gmod_get-id as key
+(define gmodule-manager (dman_data-manager gmod_get-id))
---
+++ guilecraft/problem-type-manager.scm
@@ -0,0 +1,60 @@
+;;; guilecraft --- fast learning tool.         -*- coding: utf-8 -*-
+
+;;; Commentary:
+;;
+;; Gmodule-Manager defines an interface to centrally store all known
+;; gmodules. The table is indexed on gmodule-id and it returns a
+;; gmodule object.
+;; Gmodule-Manager also defines an interface 
+
+(define-module (guilecraft problem-type-manager)
+  #:use-module (guilecraft data-manager)
+  #:export (ptm_add-problem-type
+	    ptm_get-problem-type
+
+	    ptm_assess-answer
+	    ptm_get-challenge))
+
+
+;; High Level Evaluate Answer
+;;; 
+
+(define ptm_assess-answer
+  (lambda (player-answer problem)
+    "Currently returns #t if @var{player-answer} is assessed
+successfully against @var{whirligig}'s current problem's solution.
+This is a high-level function, called directly from the UI."
+    ; Return the problem type, which is also the exposed procedure
+    ; accepting the 'assess message
+    (if (get-problem-type-provider problem)
+	((get-problem-type-provider problem) 'assess-answer problem player-answer)
+	'unknown-problem-type)))
+
+(define ptm_get-challenge
+  (lambda (problem)
+    "Return a challenge, depending on the type of problem."
+    ; Return and apply the problem-type-provider if it exists, which
+    ; is also the exposed procedure accepting the 'get-challenge
+    ; message 
+    (if (get-problem-type-provider problem)
+	((get-problem-type-provider problem) 'get-challenge problem)
+	'ptm_get-challenge-unknown-problem-type)))
+
+(define ptm_add-problem-type
+  (lambda (problem-type-provider prototype-problem-type)
+    "Convenience procedure to add a given gmodule to the
+gmodule-manager."
+    (problem-type-manager 'put problem-type-provider prototype-problem-type)))
+
+(define get-problem-type-provider
+  (lambda (problem)
+    "Convenience procedure to retrieve a given problem-type-provider
+from the problem-type-manager table."
+    (problem-type-manager 'get (ptm_get-problem-type problem))))
+
+(define ptm_get-problem-type
+  (lambda (problem)
+    (record-type-name (record-type-descriptor problem))))
+
+; define a data-manager table using the result of record-type-name as key
+(define problem-type-manager (dman_data-manager ptm_get-problem-type))
