1d0f8f5743cdfb82fa88a22b3c9c78246e0e90ce
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2013-05-03 19:35:05 +0200

    guilecraft.scm: use /usr/bin/env; fix formatting
    * guilecraft.scm: use /usr/bin/env; fix formatting

--- guilecraft.scm
+++ guilecraft.scm
@@ -1,12 +1,17 @@
-#! /usr/bin/guile -s
+#!/usr/bin/env guile
+-s
 coding:utf-8
 !#
 
-;;; Launcher sets the loadpath to include guilecraft's 
-;;; directory.
-;;; For testing:
-;;; Execute script, launch emacs, connect-to-guile, 
-;;; load-file, eval-buffer.
+;;; Commentary:
+;;
+;; Launcher sets the loadpath to include guilecraft's
+;; directory.
+;; For testing:
+;; Execute script, launch emacs, connect-to-guile,
+;; load-file, eval-buffer.
+;;
+;;; Code:
 
 (add-to-load-path "/home/alex/Projects/guilecraft/") ;location of guilecraft libs
 
