44e67e3a0d66ec2c1967cd7bff1e3bb2463376a5
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2013-05-15 22:34:11 +0200

    scorecard-ops: significant rewrite; renamed abstraction functions; added commentary.

--- guilecraft/scorecard-ops.scm
+++ guilecraft/scorecard-ops.scm
@@ -5,147 +5,200 @@
   #:use-module (srfi srfi-9)
   #:use-module (guilecraft data-types scorecards)
   #:use-module (guilecraft data-types gprofiles)
+  #:use-module (guilecraft data-types gmodules)
   #:use-module (guilecraft gprofile-ops)
   #:use-module (guilecraft gmodule-ops)
   #:use-module (guilecraft gmodule-manager)
-  #:use-module (guilecraft profiler)
-  #:export (scc_make-dummy-scorecard-datum
-	    scc_dummy-scorecard-datum?
+  #:export (make-empty-scorecard
+	    null-scorecard?
+
+	    car-gmod-blobs
+	    cdr-gmod-blobs
+	    null-gmod-blobs?
+
+	    car-gset-blobs
+	    cdr-gset-blobs
+	    null-gset-blobs?
+
+	    make-dummy-gmod-blob
+	    dummy-gmod-blob?
+
+	    make-dummy-gset-blob
+	    dummy-gset-blob?
+
+	    update-scorecard
+
 	    active-module?
 
-	    scc_first-in-scorecard
-	    scc_rest-of-scorecard
-	    scc_empty-scorecard?
-	    scc_update-scorecard
+	    lower-score?
 
-	    scc_lower-score?
+	    make-scorecard-skeleton))
 
-	    scc_make-scorecard-skeleton))
+(define (make-empty-scorecard)
+  "Return a scorecard with no scorecard-data."
+  (make-scorecard '()))
 
-(define scc_make-scorecard-skeleton
-  (lambda (active-modules)
-    "Take a list of module-ids and return a new scorecard with a
-complete set of data and scores set to 0.
+(define (null-scorecard? scorecard)
+  "Return #t if scorecard has no data."
+  (null-gmod-blobs? (scorecard-data scorecard)))
 
-Scorecards are a list, containing a scorecard-datum for each gset-tag,
-for each acive-module in the active-modules list."
-    (cond
-     ((null? active-modules)
-      '())
-     (else
-      (append
-       (add-scorecard-data (car active-modules)
-			   (gmod_get-gmodule-tags
-			    (gman_gmodule-id->gmodule-object
-			     (car active-modules))))
-       (scc_make-scorecard-skeleton (cdr active-modules)))))))
+(define (make-dummy-gset-blob)
+  "Return a score-gset-blob with no real data."
+  (make-gset-blob 'no-tag #f))
 
-(define add-scorecard-data
-  (lambda (active-module-id gset-tags)
-    (cond ((null? gset-tags)
-	   '())
-	  (else (cons (add-scorecard-datum active-module-id
-					   (car gset-tags))
-		      (add-scorecard-data active-module-id
-					  (cdr gset-tags)))))))
+(define (make-dummy-gmod-blob)
+  "Return a score-gmod-blob with only one dummy-score-gset-blob as its
+data."
+  (make-gmod-blob 'no-gmodule
+			(list (make-dummy-gset-blob))))
 
-(define add-scorecard-datum
-  (lambda (module-id set-tag)
-    (scc_make-scorecard-datum module-id set-tag 0)))
+(define (dummy-gmod-blob? score-gmod-blob)
+  "Return #t if score-gmod-blob is a dummy-gmod-blob."
+  (if (and (eq? (gmod-blob-id score-gmod-blob) 'no-gmodule)
+	   (dummy-gset-blob? (car (gmod-blob-data score-gmod-blob))))
+      #t
+      #f))
 
-(define scc_make-dummy-scorecard-datum
-  (lambda ()
-    (scc_make-scorecard-datum 'no-module 'no-set-tag #f)))
+(define (dummy-gset-blob? score-gset-blob)
+  "Return #t if score-gmod-blob is a dummy-gset-blob."
+  (if (and (eq? (gset-blob-tag score-gset-blob) 'no-tag)
+	   (not (gset-blob-score score-gset-blob)))
+      #t
+      #f))
 
-(define scc_dummy-scorecard-datum?
-  (lambda (scorecard-datum)
-    (if (and (eq? (scc_get-scorecard-datum-gmodule-id scorecard-datum)
-		  'no-module)
-	     (eq? (scc_get-scorecard-datum-gset-tag scorecard-datum)
-		  'no-set-tag)
-	     (not (scc_get-scorecard-datum-score scorecard-datum)))
-	#t
-	#f)))
+;; Data abstraction for processing gset blobs.
+(define (car-gmod-blobs scorecard-data)
+  "Return the first score-gmod-blob in scorecard-data."
+  (car scorecard-data))
+(define (cdr-gmod-blobs scorecard-data)
+  "Return the remaining score-gmod-blobs in scorecard-data."
+  (cdr scorecard-data))
+(define (null-gmod-blobs? scorecard-data)
+  "Return #t if scorecard-data contains no gmod-blobs."
+  (null? scorecard-data))
 
-(define scc_first-in-scorecard
-  (lambda (scorecard)
-    (car scorecard)))
+;; Data abstraction for processing gset blobs.
+(define (car-gset-blobs gset-blobs)
+  "Return the first gset-blob within gset-blobs."
+  (car gset-blobs))
+(define (cdr-gset-blobs gset-blobs)
+  "Return the rest of the gset-blobs in gset-blobs."
+  (cdr gset-blobs))
+(define (null-gset-blobs? gset-blobs)
+  "Return #t if gset-blobs contains no gset-blobs."
+  (null? gset-blobs))
 
-(define scc_rest-of-scorecard
-  (lambda (scorecard)
-    (cdr scorecard)))
+(define (lower-score? gset-blob1 gset-blob2)
+  "Return #t if gset-blob2 is dummy-gset-blob, or if its score is
+lower than that of gset-blob1."
+  (cond ((dummy-gset-blob? gset-blob2)
+	 #t)
+	((< (gset-blob-score gset-blob1)
+	    (gset-blob-score gset-blob2))
+	 #t)
+	(else #f)))
 
-(define scc_empty-scorecard?
-  (lambda (scorecard)
-    (null? scorecard)))
+(define (active-module? gmod-blob active-modules)
+  "Return #t if the gmod-blob-id in gmod-blob is part of the
+active-modules list."
+  (member (gmod-blob-id gmod-blob)
+	  active-modules))
 
-(define scc_lower-score?
-  (lambda (scorecard-datum1 scorecard-datum2)
-    (cond ((scc_dummy-scorecard-datum? scorecard-datum2)
-	   #t)
-	  ((< (scc_get-scorecard-datum-score scorecard-datum1)
-	      (scc_get-scorecard-datum-score scorecard-datum2))
-	   #t)
-	  (else #f))))
+(define (make-scorecard-skeleton gmodule-object-list)
+  "Return a scorecard populated with a score-gmod-blob for each
+gmodule-object, in turn containing a nil-score-gset-blob for each
+gset-tag in that gmodule-objects' gmodule-parts section."
 
-;;; Currently is spaghetti code - probably needs to be split into
-;;; separate functions and might need to be exported into a separate
-;;; module. Currently returns a new profile only., which then makes
-;;; port_portal return the new profile. port_portal should return
-;;; evaluation, as well as correct answer and new profile.
+  (define (add-scorecard-data gmodule-object-list)
+    "Return a list of gmod-blobs containing nil-score-gset-blobs for
+each gmodule-object in the list."
+    (map add-nil-score-gmod-blob gmodule-object-list))
 
-(define scc_update-scorecard
-  (lambda (old-profile assessment-result)
-    "Return a new profile, which is used to generate the next
-challenge on the basis of the existing profile and a boolean, normally
-derived from player answer assessment."
-    (let ([lowest-scoring-datum
-	   (scc_make-scorecard-datum
-	    (prof_profiler 'get-gset-gmodule old-profile)
-	    (prof_profiler 'get-gset-tag old-profile)
-	    0)])
-	(define helper
-	  (lambda (temp-scorecard lowest-scoring-datum
-				  new-scorecard)
-	    "Return a modified scorecard by recursing through tho old
-scorecard and updating the lowest scoring datum"
-	    (cond ((null? temp-scorecard)
-		   (reverse new-scorecard))
-		  ((and (eq? (scc_get-scorecard-datum-gset-tag
-			      (scc_first-in-scorecard temp-scorecard))
-			     (scc_get-scorecard-datum-gset-tag lowest-scoring-datum)))
-		   (helper (scc_rest-of-scorecard temp-scorecard)
-			   lowest-scoring-datum
-			   (cons
-			    (scc_make-scorecard-datum
-			     (scc_get-scorecard-datum-gmodule-id
-			      (scc_first-in-scorecard temp-scorecard))
-			     (scc_get-scorecard-datum-gset-tag
-			      (scc_first-in-scorecard temp-scorecard))
-			     (+ (scc_get-scorecard-datum-score
-				 (scc_first-in-scorecard temp-scorecard))
-				1))
-			    new-scorecard)))
-		  (else (helper (scc_rest-of-scorecard temp-scorecard)
-				lowest-scoring-datum
-				(cons (scc_first-in-scorecard
-				       temp-scorecard)
-				      new-scorecard))))))
+  (define (add-nil-score-gmod-blob gmodule-object)
+    "Return a gmod-blob containing a nil-score gset-blob for each
+gset-tag in the gmodule object."
+    (make-gmod-blob (gmodule-id gmodule-object)
+			  (add-nil-score-gset-blobs (gmodule-tags
+						     gmodule-object))))
+  (define (add-nil-score-gset-blobs gset-tags)
+    "Return a list of nil-score-gset-blobs for each tag in the
+gset-tags list."
+    (map add-nil-score-gset-blob gset-tags))
 
-	(cond ((eq? assessment-result #t)
-	       (let ([%name (gprof_get-name old-profile)]
-		     [%id (gprof_update-id old-profile)]
-		     [%active-modules (gprof_get-active-modules old-profile)])
-		 (gprof_make-profile
-		  (name %name)
-		  (id %id)
-		  (active-modules %active-modules)
-		  (scorecard (helper (gprof_get-scorecard old-profile)
-				     lowest-scoring-datum
-				     '())))))
-	      (else old-profile)))))
+  (define add-nil-score-gset-blob
+    (lambda (gset-tag)
+      "Return a newly created gset-blob of gset-tag with score 0."
+      (make-gset-blob gset-tag 0)))
 
-(define active-module?
-  (lambda (scorecard-datum active-modules)
-    (member (scc_get-scorecard-datum-gmodule-id scorecard-datum)
-	    active-modules)))
+  ;; Return the scorecard-skeleton
+  (make-scorecard (add-scorecard-data gmodule-object-list)))
+
+
+(define (update-scorecard scorecard gmodule-id gset-tag assessment-result)
+  "Return a new scorecard, with the scores of each gset-blob adjusted
+based on assessment-result."
+
+  (define (modify-score old-score assessment-result)
+    "Returns a score modified by an algorithm on the basis of
+assessment-result, to take the place of old-score."
+    (if assessment-result
+	(+ old-score 1)
+	old-score))
+
+  (define (gset-blobs-worker old-gset-blobs gset-tag assessment-result
+			     new-gset-blobs)
+    "Returns a list of gset-blobs with the gset-blob whose tag matches
+gset-tag having its score modified by modify-score."
+    (cond ((null? old-gset-blobs)
+	   (reverse new-gset-blobs))
+	  ((eq? (gset-blob-tag (car-gset-blobs old-gset-blobs))
+		gset-tag)
+	   (gset-blobs-worker (cdr-gset-blobs old-gset-blobs)
+			      gset-tag
+			      assessment-result
+			      (cons (make-gset-blob
+				     gset-tag
+				     (modify-score (gset-blob-score
+						    (car-gset-blobs old-gset-blobs))
+						   assessment-result))
+				    new-gset-blobs)))
+	  (else (gset-blobs-worker (cdr old-gset-blobs)
+				   gset-tag
+				   assessment-result
+				   (cons (car old-gset-blobs)
+					 new-gset-blobs)))))
+
+  (define (gmod-blobs-worker old-gmod-blobs gmodule-id gset-tag
+			     assessment-result new-gmod-blobs)
+    "Returns a list of gmod-blobs with the gmod-blob whose id matches
+gmodule-id having its gset-blobs modified by gset-blobs-worker."
+    (cond ((null? old-gmod-blobs)
+	   (reverse new-gmod-blobs))
+	  ((eq? (gmod-blob-id (car old-gmod-blobs))
+		gmodule-id)
+	   (gmod-blobs-worker (cdr old-gmod-blobs)
+			      gmodule-id
+			      gset-tag
+			      assessment-result
+			      (cons
+			       (make-gmod-blob
+				gmodule-id
+				(gset-blobs-worker
+				 (gmod-blob-data (car old-gmod-blobs))
+				 gset-tag
+				 assessment-result
+				 '()))
+			       new-gmod-blobs)))
+	  (else (gmod-blobs-worker (cdr old-gmod-blobs)
+				   gmodule-id
+				   gset-tag
+				   assessment-result
+				   (cons (car old-gmod-blobs)
+					 new-gmod-blobs)))))
+
+  ;; Return the updated scorecard
+  (make-scorecard (gmod-blobs-worker (scorecard-data scorecard)
+				     gmodule-id
+				     gset-tag
+				     assessment-result
+				     '())))
