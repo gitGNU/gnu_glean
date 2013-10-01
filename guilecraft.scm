148a001a00ac4f99897f4742f1072ce7714579a9
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2013-10-01 10:04:56 +0200

    Add final launcher.

---
+++ guilecraft.scm
@@ -0,0 +1,18 @@
+#!/usr/bin/env guile
+-s
+coding:utf-8
+!#
+
+;;; Commentary:
+;;
+;; Launcher sets the loadpath to include guilecraft's
+;; directory.
+;; For testing:
+;; Execute script, launch emacs, connect-to-guile,
+;; load-file, eval-buffer.
+;;
+;;; Code:
+
+(add-to-load-path (dirname (current-filename))) ;location of guilecraft libs
+
+((@ (guilecraft boot) boot) (program-arguments))
