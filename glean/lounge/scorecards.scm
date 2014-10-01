9ea8929ffb06723b3cf78de79317c4543a943e1e
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2014-10-01 14:05:20 +0200

    Scorecards: apply common style, re-organize.
    * Makefile.am: remove data-manager.scm.
    * glean/lounge/data-manager.scm: merged with scorecards, as otherwise
      obsolete.
    * glean/lounge/scorecards.scm: apply common style, improve documentation.
      (check-blob) (add-blob) (extend-scorecard) (scorecard-first)
      (scorecard-rest) (active-module?) (make-scorecard-skeleton): remove
      procedures.
      (data-manager): new procedure.

--- glean/lounge/scorecards.scm
+++ glean/lounge/scorecards.scm
@@ -1,14 +1,58 @@
-;;; glean --- Fast learning tool.         -*- coding: utf-8 -*-
+;; scorecards.scm --- scorecard data type definition   -*- coding: utf-8 -*-
+;;
+;; Copyright (C) 2014 Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
+;;
+;; Author: Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
+;; Created: 01 January 2014
+;;
+;; This file is part of Glean.
+;;
+;; Glean is free software; you can redistribute it and/or modify it under the
+;; terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; Glean is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with glean; if not, contact:
+;;
+;; Free Software Foundation           Voice:  +1-617-542-5942
+;; 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
+;; Boston, MA  02111-1307,  USA       gnu@gnu.org
+
+;;; Commentary:
+;;
+;; Scorecards contain the results of player challenge responses, and are thus
+;; the key data type in the lounge.
+;;
+;; A scorecard contains many blobs, which are the record-type that actually
+;; tracks progress. Blobs also contain within them the abstract representation
+;; of disciplines, through the use of parent and children fields.
+;;
+;; This currently means that that abstract representation is duplicated for
+;; each profile stored in the lounge.  It also feels like a serious blurring
+;; of concerns.  I would like to split the abstract representation into a
+;; separate lounge module, and merely use score-cards to track individual
+;; results (referring to the abstract representation module as and when
+;; necessary).
+;;
+;; They are currently implemented as a hashtable contained in a 1 field record
+;; — both should change.
+;;
+;;; Code:
 
 (define-module (glean lounge scorecards)
   #:use-module (glean common utils)
-  #:use-module (glean library sets)      ; obsolete?
-  #:use-module (glean lounge data-manager)
-  #:use-module (rnrs)
+  #:use-module (rnrs records procedural)
   #:use-module (srfi srfi-1)
-  #:export (make-scorecard
+  #:export (
+            make-scorecard
 	    scorecard?
-	    scorecard-data
+ 	    scorecard-data
 
 	    make-blob
 	    blob?
@@ -22,25 +66,20 @@
 
 	    make-empty-scorecard
 	    empty-scorecard?
-	    extend-scorecard
-
-	    scorecard-first
-	    scorecard-rest
-
+            
 	    make-dummy-blob
 	    dummy-blob?
 	    find-blob
-	    check-blob
-	    add-blob
-	    add-blobs
+ 	    add-blobs
 
 	    update-scorecard
 
-	    active-module?
-	    lower-score?
+ 	    lower-score?
 	    blobhash?
+            ))
 
-	    make-scorecard-skeleton))
+
+;;;;; Record Type Definitions
 
 (define blob-rtd
   (make-record-type-descriptor 'blob #f #f #f #f
@@ -71,9 +110,9 @@
 (define make-scorecard (record-constructor scorecard-rcd))
 (define scorecard? (record-predicate scorecard-rtd))
 (define scorecard-data (record-accessor scorecard-rtd 0))
+
 
-;;;; Scorecard Operations
-(define (blobhash? obj) (symbol? obj))
+;;;;; Scorecard Operations
 
 (define (find-blob blobhash scorecard)
   "Return the blob stored under BLOBHASH in SCORECARD."
@@ -82,16 +121,6 @@
       (data 'values))
     ((scorecard-data scorecard) 'get blobhash)))
 
-(define (check-blob blobhash scorecard)
-  "Return #t if SCORECARD contains BLOBHASH, otherwise #f."
-  ((scorecard-data scorecard) 'contains blobhash))
-
-(define (add-blob blob scorecard)
-  "Return BLOB if we have been able to add BLOB, indexed by its
-blobhash, in SCORECARD. Otherwise return #f."
-  (let ((result ((scorecard-data scorecard)
-		 'put (blob-hash blob) blob)))
-    (if result result #f)))
 (define (add-blobs blobs scorecard)
   "Return a new scorecard created by adding BLOBS to SCORECARD."
   (let ((data (scorecard-data scorecard)))
@@ -107,25 +136,78 @@ blobhash, in SCORECARD. Otherwise return #f."
   "Return #t if scorecard has no data."
   (null? (vector->list ((scorecard-data scorecard) 'list-keys))))
 
+(define (update-scorecard scorecard blobhash assessment-result)
+  "Return a new scorecard, on the basis of SCORECARD, with the score
+of the blob identified by BLOBHASH, and its parents, adjusted based on
+ASSESSMENT-RESULT."
+  (let* ((data (scorecard-data scorecard))
+         (initial-blob (data 'get blobhash)))
+
+    (define (update-skorecard data blobhash)
+      (let ((blob (data 'get blobhash)))
+        (define (update-data)
+          (let ((new-blob (update-blob blob assessment-result initial-blob
+                                       (number-of-child-blobs blob scorecard))))
+            (data 'put blobhash new-blob)
+            data))
+        (if (null? (blob-parents blob))
+            (update-data)
+            ;; INFO: Currently this only modifies the car of parents,
+            ;; which could conceivably contain more than one element. We
+            ;; may need to process the other parents too… perhaps.
+            (update-skorecard (update-data) (car (blob-parents blob))))))
+
+    (make-scorecard (update-skorecard data blobhash))))
+
+
+;;;;; Blob Operations
+
+(define (blobhash? obj)
+  "Return #t if OBJ is a blobhash, #f otherwise."
+  (symbol? obj))
+
+(define (update-blob blob assessment-result initial-blob
+                     number-of-child-blobs)
+  "Return a new blob constructed on the basis of BLOB, with its score
+adapted according to ASSESSMENT-RESULT, its counter progressed, and its
+effects updated if INITIAL-BLOB contains 'tutorial key.."
+  (make-blob (blob-hash blob)
+	     (blob-parents blob)
+	     (blob-children blob)
+             (modify-score (blob-score blob)
+                           assessment-result
+                           number-of-child-blobs)
+             (progress-counter (blob-counter blob)
+                               assessment-result)
+             (blob-properties blob)
+             (consider-effects/properties blob assessment-result initial-blob
+                                          number-of-child-blobs)))
+
 (define (make-dummy-blob)
   "Return a score-blob with no real data."
   (make-blob 'no-tag '() '() #f 0 '() '()))
 
 (define (dummy-blob? score-blob)
-  "Return #t if score-mod-blob is a dummy-blob."
+  "Return #t if score-mod-blob is a dummy-blob, #f otherwise."
   (if (and (eq? (blob-hash score-blob) 'no-tag)
 	   (not (blob-score score-blob))
 	   (zero? (blob-counter score-blob)))
       #t
       #f))
 
-;; ;; Data abstraction for processing gset blobs.
-;; (define (scorecard-first scorecard)
-;;   "Return the first blob within blobs."
-;;   (car (scorecard-data scorecard)))
-;; (define (scorecard-rest scorecard)
-;;   "Return the rest of the blobs in blobs."
-;;   (cdr (scorecard-data scorecard)))
+(define (modify-score old-score assessment-result number-of-child-blobs)
+  "Returns a score modified by an algorithm on the basis of
+assessment-result, to take the place of old-score."
+  (if assessment-result
+      (+ old-score (if (< 0 number-of-child-blobs)
+                       (/ 1 number-of-child-blobs)
+                       1))
+      old-score))
+
+(define (progress-counter old-counter-value assessment-result)
+  "Returns the increment of counter-value or 0 if counter-value
+is equal to or greater the number of problems in the gset."
+  (1+ old-counter-value))
 
 (define (lower-score? blob1 blob2)
   "Return #t if blob1's score is lower than that of blob2, or if blob1 has the
@@ -153,49 +235,6 @@ larger than blob2."
     (if log (format #t "~a\n" (car res)))
     (cdr res)))
 
-(define (property-or-effect? key blob)
-  "Return #t if property or effect identified by KEY is active in BLOB, #f
-otherwise."
-  (let ((prop (assoc key (blob-properties blob)))
-        (effect (assoc key (blob-effects blob))))
-    (or (and (pair? prop) (cdr prop))
-        (and (pair? effect) (cdr effect)))))
-
-(define (active-module? crown-blob active-modules)
-  "Return #t if the blob-hash in crown-blob is part of the
-active-modules list."
-  (member (blob-hash crown-blob)
-	  active-modules))
-
-(define (extend-scorecard blobs scorecard)
-  "Return a new scorecard by merging BLOBS into SCORECARD."
-  (define (fold-blobs result-so-far blob)
-    (if (and result-so-far (add-blob blob scorecard)) #t #f))
-  (fold-left fold-blobs #t blobs))
-
-(define (update-scorecard scorecard blobhash assessment-result)
-  "Return a new scorecard, on the basis of SCORECARD, with the score
-of the blob identified by BLOBHASH, and its parents, adjusted based on
-ASSESSMENT-RESULT."
-  (let* ((data (scorecard-data scorecard))
-         (initial-blob (data 'get blobhash)))
-
-    (define (update-skorecard data blobhash)
-      (let ((blob (data 'get blobhash)))
-        (define (update-data)
-          (let ((new-blob (update-blob blob assessment-result initial-blob
-                                       (number-of-child-blobs blob scorecard))))
-            (data 'put blobhash new-blob)
-            data))
-        (if (null? (blob-parents blob))
-            (update-data)
-            ;; INFO: Currently this only modifies the car of parents,
-            ;; which could conceivably contain more than one element. We
-            ;; may need to process the other parents too… perhaps.
-            (update-skorecard (update-data) (car (blob-parents blob))))))
-
-    (make-scorecard (update-skorecard data blobhash))))
-
 (define number-of-child-blobs
   (let ((previous (make-hash-table)))
     (lambda (blob scorecard)
@@ -220,22 +259,16 @@ same inputs."
             (num-of-child-blobs-helper (blob-children blob) 0)
             (error 'number-of-child-blobs "Blob or data not right."))))))
 
-(define (update-blob blob assessment-result initial-blob
-                     number-of-child-blobs)
-  "Return a new blob constructed on the basis of BLOB, with its score
-adapted according to ASSESSMENT-RESULT, its counter progressed, and its
-effects updated if INITIAL-BLOB contains 'tutorial key.."
-  (make-blob (blob-hash blob)
-	     (blob-parents blob)
-	     (blob-children blob)
-             (modify-score (blob-score blob)
-                           assessment-result
-                           number-of-child-blobs)
-             (progress-counter (blob-counter blob)
-                               assessment-result)
-             (blob-properties blob)
-             (consider-effects/properties blob assessment-result initial-blob
-                                          number-of-child-blobs)))
+
+;;;;; Properties and/or Effects
+
+(define (property-or-effect? key blob)
+  "Return #t if property or effect identified by KEY is active in BLOB, #f
+otherwise."
+  (let ((prop (assoc key (blob-properties blob)))
+        (effect (assoc key (blob-effects blob))))
+    (or (and (pair? prop) (cdr prop))
+        (and (pair? effect) (cdr effect)))))
 
 (define (consider-effects/properties blob assessment-result initial-blob
                                      number-of-child-blobs)
@@ -267,22 +300,12 @@ KEY. Return BLOB-EFFECTS otherwise."
                 blob-effects)
         blob-effects)))
 
-(define (modify-score old-score assessment-result number-of-child-blobs)
-  "Returns a score modified by an algorithm on the basis of
-assessment-result, to take the place of old-score."
-  (if assessment-result
-      (+ old-score (if (< 0 number-of-child-blobs)
-                       (/ 1 number-of-child-blobs)
-                       1))
-      old-score))
-
-(define (progress-counter old-counter-value assessment-result)
-  "Returns the increment of counter-value or 0 if counter-value
-is equal to or greater the number of problems in the gset."
-  (1+ old-counter-value))
-
-;;;;; Tutorial Property Considerations
-(define (blob-score-completed-tutorial? score) (>= score 1))
+
+;;;; Tutorial Property Considerations
+(define (blob-score-completed-tutorial? score)
+  "Return #t if SCORE is considered to reflect a completed tutorial.  At
+present this is the case if SCORE is greater than or equal 1."
+  (>= score 1))
 
 (define (complete-tutorial? blob)
   "Return #t if BLOB is a tutorial and it is \"complete\", i.e. it and all
@@ -302,3 +325,61 @@ otherwise."
   (and (property-or-effect? 'tutorial blob) ; is tutorial
        (> (blob-score blob) 0)              ; tutorial started
        (incomplete-tutorial? blob)))        ; tutorial not complete.
+
+
+;;;;; Data Manager
+;; FIXME: THIS IS NON-FUNCTIONAL!  Only used by scorecards.
+
+(define data-manager
+  (lambda (predicate)
+    "An old-school, non-functional (hash-table based) non-persistent
+data-store to store scorecard data.  This is crufty, and should be abandoned
+ASAP!"
+
+    (let ([data (make-eqv-hashtable)])
+      (lambda (message . args)
+	"Return the value associated with (car ARGS) if MESSAGE is
+'get, or #f if the (car ARGS) is not a key in the hashtable.
+
+ Return #t if MESSAGE is 'put, and (cadr ARGS) passes the predicate
+check and is added to table. Return #f otherwise.
+
+Return all values stored in the table when MESSAGE is 'list."
+
+	(cond ((eqv? message 'get)      ; Retrieving an entry
+	       (hashtable-ref data (car args) #f))
+	      ((eqv? message 'put)	; Storing an entry
+	       (if (predicate (cadr args))
+		   (begin
+		     (hashtable-set! data (car args) (cadr args))
+		     #t)
+		   #f))
+              ((eqv? message 'rem-val)
+               (call-with-values
+                   ;; fetch values and keys
+                   (lambda () (hashtable-entries data))
+                 (lambda (ks vs)
+                   (vector-for-each
+                    ;; destroy every entry who's value matches arg to
+                    ;; 'rem-val
+                    (lambda (k v)
+                      (if (equal? v (car args))
+                          (hashtable-delete! data k)))
+                    ks vs))))
+	      ((eqv? message 'rem)
+	       (hashtable-delete! data (car args)))
+	      ((eqv? message 'list-keys (hashtable-keys data)))
+	      ((eqv? message 'list) (hashtable-entries data))
+	      ((eqv? message 'values)
+	       (vector->list
+		(vector-map (lambda (key)
+			      (hashtable-ref data key #f))
+			    (hashtable-keys data))))
+	      ((eqv? message 'contains)
+	       (hashtable-contains? data (car args)))
+	      ((eqv? message 'update)
+	       (hashtable-update! data (car args)
+				  (cadr args) (caddr args)))
+	      (else (error "data-manager: unknown message: " message)))))))
+
+;;; scorecards.scm ends here
