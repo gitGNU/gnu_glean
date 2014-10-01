0ec12bc433b916128bc5210a1653ef25c14d6bb3
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2014-10-01 12:40:35 +0200

    Lounge-filesystem: apply common style.
    * glean/lounge/lounge-filesystem.scm: apply common style, improve
      documentation.

--- glean/lounge/lounge-filesystem.scm
+++ glean/lounge/lounge-filesystem.scm
@@ -1,97 +1,89 @@
-;;; glean --- fast learning tool.         -*- coding: utf-8 -*-
-
-;;;; Lounge Filesystem — Lounge-store → Filesystem Interface
-
-;; Copyright © 2012, 2014 Alex Sassmannshausen
+;; lounge-filesystem.scm --- Lounge-store → Filesystem  -*- coding: utf-8 -*-
 ;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3 of
-;; the License, or (at your option) any later version.
+;; Copyright (C) 2014 Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
 ;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Author: Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
+;; Created: 01 January 2014
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, contact:
+;; This file is part of Glean.
+;;
+;; Glean is free software; you can redistribute it and/or modify it under the
+;; terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; Glean is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with glean; if not, contact:
 ;;
 ;; Free Software Foundation           Voice:  +1-617-542-5942
 ;; 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
 ;; Boston, MA  02111-1307,  USA       gnu@gnu.org
 
-;;;; Commentary:
-;;;
-;;; Provide 'compile-lounge' and 'write-pdiff', 2 procedures that
-;;; should handle all i/o to the underlying storage media.
-;;;
-;;;; Code:
+;;; Commentary:
+;;
+;; Provide `compile-lounge' and `write-pdiff', 2 procedures that
+;; should handle all i/o to the underlying storage media.
+;;
+;; This is a first version of an interface to persistent storage.  My aim is
+;; to implement persistent storage as a 'component', so that different core
+;; and extension mechanisms can be used as drop in replacements.  At present
+;; the minimum interface provided by those components is `compile-lounge',
+;; which should return a complete lounge catalogue as represented in the
+;; file-system, and `write-diff', a procedure that is given a lounge
+;; transaction, which should ensure that that transaction is recorded in a
+;; persistent manner, for use by the `compile-lounge' procedure.
+;;
+;; Future interface additions could include statistical and export/import
+;; functionality.
+;;
+;; A good argument could be made that `compile-lounge' is to general: one
+;; could imagine a sql based backend which would suffer from having to simply
+;; dump the entire store into a vhash data-structure.  A more realistic
+;; approach in future might be to require 'goal' oriented interface
+;; requirements, which are then implemented at component level
+;; (e.g. `fetch-profile-summary', `fetch-active-modules', etc.).
+;;
+;;; Code:
 
 (define-module (glean lounge lounge-filesystem)
-  #:use-module (glean config)
-  #:use-module (glean common monads)
   #:use-module (glean common utils)
-  #:use-module (glean lounge gprofiles)
   #:use-module (glean lounge lounge-store)
   #:use-module (ice-9 ftw)
   #:use-module (ice-9 vlist)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
-  #:export (compile-lounge
-            write-diff))
+  #:export (
+            compile-lounge
+            write-diff
+            ))
 
-(define (make-brick diff)
-  "A general brick. This could probably be killed. But does it add
-clarity?"
-  (lambda (lng-dir time)
-    (match diff
-      (('diff hash field value)
-       (let ((place (string-append lng-dir "/" hash)))
-         (mkdir-p place)
-         (mkdir-p (string-append place "/actives"))
-         (mkdir-p (string-append place "/counters"))
-         (brick-write place diff time field))))))
+
+;;;;; Overview
+;;; The persistent storage implemented by this module is a plain folder/file
+;;; system.  Individual transactions are recorded in the subdirectory of it's
+;;; profile, one transaction per file.
+;;;
+;;; `compile-lounge' traverses the filesystem tree and reconstitutes the
+;;; profiles that have been stored by 'replaying' each transaction.
 
-(define (brick-write profile-dir diff time field)
-  "A procedure to ensure CONTENTS that are valid get written to
-PLACE."
-  (let ((place (string-append profile-dir "/" (number->string time)
-                              "-" (symbol->string field))))
-    (define (make-counter counter)
-      (let ((suffix (string-append "-" (number->string counter))))
-        (if (access? (string-append place suffix) R_OK)
-            (make-counter (1+ counter))
-            suffix)))
+;;;;; Porcelain
 
-    (with-output-to-file (string-append place (make-counter 0))
-      (lambda ()
-        (write (list diff time))
-        (newline)))))
-
-(define (fire-kiln brick lng-dir time)
-  "Ensure the folder identified by the string HASH exists in LNG-DIR,
-then ask BRICK to write itself to that folder, against TIME."
-  (brick lng-dir time))
-
+;;;; Write-diff
 (define (write-diff diff lng-dir time)
-  "Use HASH, PROFILE, FIELD, VALUE and OLDHASH to determine what needs
-to be written to disk in LNG-DIR. The return value is unspecified."
+  "Write the tagged list DIFF to the appropriate folder in LNG-DIR (a string),
+with timestamp TIME. The return value is unspecified."
   (fire-kiln (make-brick diff) lng-dir time))
 
-
 ;;;; Compile Lounge: initial loading of previous session data.
-;;; As we have stored the data as diffs enclosed in a list, loading the data
-;;; should be simple: simply read the data and pass the diff and pass it to
-;;; the normal processing procedure for a new diff: update-lounge.
-;;;
-;;; General Method:
-;;; 
-;;; - Traverse lounge dir.
-;;; - For each hash-dir encountered, traverse it, open each file (ignoring
-;;; folders for now), read list, extract diff and pass to update-lounge.
-;;; - Move to next hash.
 (define (compile-lounge lng-dir)
+  "Traverse the directory identified by the string LNG-DIR, interpreting each
+top level folder as a profile, and its name as a hash.  Each file within each
+of these folders is considered a transaction to be 'replayed'."
   (define (enter? . args) #t)
   (define (leaf path stat result)
     (cons path result))
@@ -116,3 +108,42 @@ to be written to disk in LNG-DIR. The return value is unspecified."
                            (string<? (basename filepath1)
                                      (basename filepath2))))))))
 
+
+;;;; Helpers
+;;; These are all related to write-diff.
+
+(define (make-brick diff)
+  "Return a procedure which, when given a location and a timestamp, will write
+the tagged list DIFF to the file-system."
+  (lambda (lng-dir time)
+    (match diff
+      (('diff hash field value)
+       (let ((place (string-append lng-dir "/" hash)))
+         (mkdir-p place)
+         (mkdir-p (string-append place "/actives"))
+         (mkdir-p (string-append place "/counters"))
+         (brick-write place diff time field))))))
+
+(define (brick-write profile-dir diff time field)
+  "Write the tagged list DIFF to a file named by the string PROFILE-DIR, the
+numeric timestamp TIME, the symbol FIELD and a counter generated on the fly,
+to the file-system."
+  (let ((place (string-append profile-dir "/" (number->string time)
+                              "-" (symbol->string field))))
+    (define (make-counter counter)
+      (let ((suffix (string-append "-" (number->string counter))))
+        (if (access? (string-append place suffix) R_OK)
+            (make-counter (1+ counter))
+            suffix)))
+
+    (with-output-to-file (string-append place (make-counter 0))
+      (lambda ()
+        (write (list diff time))
+        (newline)))))
+
+(define (fire-kiln brick lng-dir time)
+  "Apply BRICK to LNG-DIR and TIME.  The return value is unspecified, but the
+side-effect should be the writing of a transaction contained in brick to the file-system."
+  (brick lng-dir time))
+
+;;; lounge-filesystem.scm ends here
