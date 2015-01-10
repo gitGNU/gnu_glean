cb897f91807141ba89498d8b61d242ce30b0eb1e
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2015-01-11 00:09:23 +0100

    infrastructure: add more indent rules for srfi-64.
    * .dir-locals.el (test-error) (test-eqv): add align rules.

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,6 +7,8 @@
   .
   ((indent-tabs-mode . nil)
    (eval . (put 'test-assert 'scheme-indent-function 1))
+   (eval . (put 'test-error 'scheme-indent-function 1))
+   (eval . (put 'test-eqv 'scheme-indent-function 1))
    (eval . (put 'test-equal 'scheme-indent-function 1))
    (eval . (put 'test-eq 'scheme-indent-function 1))
    (eval . (put 'call-with-input-string 'scheme-indent-function 1))
